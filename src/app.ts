e07a75eaa857fecb04ce652810f293db03e35f71
noy-v <noyvilkin@cs.colman.ac.il>
2026-01-04 17:38:08 +0200

    implemented CRUD for user model

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import postRoutes from "./routes/post_routes";
+import userRoutes from "./routes/user_routes";
 
 // Load environment variables
 dotenv.config();
@@ -13,6 +14,7 @@ const app: Express = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/post", postRoutes);
+app.use("/user", userRoutes);
 
 // Database Connection
 const db = mongoose.connection;
---
+++ src/models/user_model.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IUser extends Document {
+    username: string;
+    email: string;
+    password: string;
+}
+
+const userSchema = new Schema<IUser>({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+export default mongoose.model<IUser>("Users", userSchema);
